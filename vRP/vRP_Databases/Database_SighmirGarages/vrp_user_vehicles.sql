fc65658536a4d52fc325b1271c9ea75eac711474
KilobaiD <mdzzalex@gmail.com>
2018-03-21 20:39:43 +0200

    Added .sql

---
+++ vRP/vRP_Databases/Database_SighmirGarages/vrp_user_vehicles.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 21 Mar 2018 la 17:22
+-- Versiune server: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vrpfx`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `vrp_user_vehicles`
+--
+
+CREATE TABLE `vrp_user_vehicles` (
+  `user_id` int(11) NOT NULL,
+  `vehicle` varchar(100) NOT NULL,
+  `veh_type` varchar(255) NOT NULL DEFAULT 'default',
+  `vehicle_plate` varchar(255) NOT NULL,
+  `vehicle_colorprimary` varchar(255) DEFAULT NULL,
+  `vehicle_colorsecondary` varchar(255) DEFAULT NULL,
+  `vehicle_pearlescentcolor` varchar(255) DEFAULT NULL,
+  `vehicle_wheelcolor` varchar(255) DEFAULT NULL,
+  `vehicle_plateindex` varchar(255) DEFAULT NULL,
+  `vehicle_neoncolor1` varchar(255) DEFAULT NULL,
+  `vehicle_neoncolor2` varchar(255) DEFAULT NULL,
+  `vehicle_neoncolor3` varchar(255) DEFAULT NULL,
+  `vehicle_windowtint` varchar(255) DEFAULT NULL,
+  `vehicle_wheeltype` varchar(255) DEFAULT NULL,
+  `vehicle_mods0` varchar(255) DEFAULT NULL,
+  `vehicle_mods1` varchar(255) DEFAULT NULL,
+  `vehicle_mods2` varchar(255) DEFAULT NULL,
+  `vehicle_mods3` varchar(255) DEFAULT NULL,
+  `vehicle_mods4` varchar(255) DEFAULT NULL,
+  `vehicle_mods5` varchar(255) DEFAULT NULL,
+  `vehicle_mods6` varchar(255) DEFAULT NULL,
+  `vehicle_mods7` varchar(255) DEFAULT NULL,
+  `vehicle_mods8` varchar(255) DEFAULT NULL,
+  `vehicle_mods9` varchar(255) DEFAULT NULL,
+  `vehicle_mods10` varchar(255) DEFAULT NULL,
+  `vehicle_mods11` varchar(255) DEFAULT NULL,
+  `vehicle_mods12` varchar(255) DEFAULT NULL,
+  `vehicle_mods13` varchar(255) DEFAULT NULL,
+  `vehicle_mods14` varchar(255) DEFAULT NULL,
+  `vehicle_mods15` varchar(255) DEFAULT NULL,
+  `vehicle_mods16` varchar(255) DEFAULT NULL,
+  `vehicle_turbo` varchar(255) NOT NULL DEFAULT 'off',
+  `vehicle_tiresmoke` varchar(255) NOT NULL DEFAULT 'off',
+  `vehicle_xenon` varchar(255) NOT NULL DEFAULT 'off',
+  `vehicle_mods23` varchar(255) DEFAULT NULL,
+  `vehicle_mods24` varchar(255) DEFAULT NULL,
+  `vehicle_neon0` varchar(255) DEFAULT NULL,
+  `vehicle_neon1` varchar(255) DEFAULT NULL,
+  `vehicle_neon2` varchar(255) DEFAULT NULL,
+  `vehicle_neon3` varchar(255) DEFAULT NULL,
+  `vehicle_bulletproof` varchar(255) DEFAULT NULL,
+  `vehicle_smokecolor1` varchar(255) DEFAULT NULL,
+  `vehicle_smokecolor2` varchar(255) DEFAULT NULL,
+  `vehicle_smokecolor3` varchar(255) DEFAULT NULL,
+  `vehicle_modvariation` varchar(255) NOT NULL DEFAULT 'off'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `vrp_user_vehicles`
+--
+ALTER TABLE `vrp_user_vehicles`
+  ADD PRIMARY KEY (`user_id`,`vehicle`);
+
+--
+-- Restrictii pentru tabele sterse
+--
+
+--
+-- Restrictii pentru tabele `vrp_user_vehicles`
+--
+ALTER TABLE `vrp_user_vehicles`
+  ADD CONSTRAINT `fk_user_vehicles_users` FOREIGN KEY (`user_id`) REFERENCES `vrp_users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ vRP/vRP_Databases/README.md
@@ -0,0 +1,7 @@
+# vrp_user_vehicles
+
+Replace your vrp_user_vehicles into your database with this, for properly working.
+
+
+
+
